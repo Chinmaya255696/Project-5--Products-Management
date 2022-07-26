122e3163e45f5cb6a4664622af5aaa31ea90137c
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-07-25 19:50:25 -0700

    update route.js

--- src/route/route.js
+++ src/route/route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router=express.Router();
 const userController = require("../controller/userController")
 
+router.post("/createUser",userController.createUser)
 router.post("/login",userController.loginUser)
+router.get("/getUser",userController.getUser)
 
 
 
